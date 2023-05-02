7c9700b45b4b279f223364066d36baa43e98fa86
Taerogrammer <hyung54321@naver.com>
2023-05-02 11:46:34 +0900

    first commit

---
+++ Mc2-Team12-DreamLog/Mc2-Team12-DreamLog/Mc2_Team12_DreamLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mc2_Team12_DreamLogApp.swift
+//  Mc2-Team12-DreamLog
+//
+//  Created by KimTaeHyung on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct Mc2_Team12_DreamLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
